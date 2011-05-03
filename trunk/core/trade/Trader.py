0e527395a089b09f7ec643d57a954f6d1ea1ca91
benjamin.dehaye@gmail.com <benjamin.dehaye@gmail.com@c5b24106-d323-f95c-f5fa-74f5891a77ab>
2011-05-03 09:02:25 +0000

    Adding of a boolean test to check wether the trader can trade or not.

--- trunk/core/trade/Trader.py
+++ trunk/core/trade/Trader.py
@@ -18,7 +18,7 @@ class Trader(object):
     '''
 
 
-    def __init__(self, accounts, stocks, resources_to_buy, resources_to_sell):
+    def __init__(self, accounts, stocks, resources_to_buy, resources_to_sell, can_trade = False):
         '''
         Constructor
         '''
@@ -26,6 +26,7 @@ class Trader(object):
         self.stocks = stocks #should be the stocks of the ship
         self.resources_to_buy = resources_to_buy #to be defined by the player or the AI
         self.resources_to_sell = resources_to_sell #to be defined by the player or the AI
+	self.can_trade = can_trade #a trader can only trade if he/she is docked to a station/factory/...
         
     def __str__(self):
         return self.accounts + " " + self.stocks + " " + self.resources_to_buy + " " + self.resources_to_sell
@@ -41,6 +42,9 @@ class Trader(object):
     
     def get_resources_to_sell(self):
         return self.resources_to_sell
+
+    def get_can_trade(self):
+	return self.can_trade
     
     def set_accounts(self, accounts):
         self.accounts = accounts
@@ -53,16 +57,19 @@ class Trader(object):
         
     def set_resources_to_sell(self, resources_to_sell):
         self.resources_to_sell = resources_to_sell
+
+    def set_can_trade(self, can_trade):
+	self.can_trade = can_trade
         
     def buy(self, resource, quantity):
-        if resource.price()*quantity <= self.accounts.get_credits() and quantity + self.stocks.get_stock_for_resource(resource) <= self.stocks.get_max_stock_for_resource(resource):
+        if self.get_can_trade() and resource.price()*quantity <= self.accounts.get_credits() and quantity + self.stocks.get_stock_for_resource(resource) <= self.stocks.get_max_stock_for_resource(resource):
             self.accounts.decrease(resource.price()*quantity)
             self.stocks.increase_resource(resource, quantity)
         else:
             raise Exception("Can not buy this resource.")
         
     def sell(self, resource, quantity):
-        if quantity < self.stocks.get_stock_for_resource(resource):
+        if self.get_can_trade() and quantity < self.stocks.get_stock_for_resource(resource):
             self.accounts.increase(resource.price()*quantity)
             self.stocks.decrease_resource(resource, quantity)
         else:

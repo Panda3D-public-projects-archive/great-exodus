9f2fe04208223b98440c96a23381bed645330576
benjamin.dehaye@gmail.com <benjamin.dehaye@gmail.com@c5b24106-d323-f95c-f5fa-74f5891a77ab>
2011-05-03 19:31:05 +0000

    Failure after refactoring...

--- trunk/core/trade/Trader.py
+++ trunk/core/trade/Trader.py
@@ -6,6 +6,7 @@ Created on 2 mai 2011
 from trunk.core.ResourcesPackage.Stock import *
 from trunk.core.trade.Accounts import *
 from trunk.core.trade.StockExchange import *
+from trunk.core.trade.StorageRoom import * #will be in "space"
 
 class Trader(object):
     '''
@@ -17,61 +18,60 @@ class Trader(object):
     to a planet or an entity that can buy/sell resources. /!\
     '''
 
-
-    def __init__(self, accounts, stocks, resources_to_buy, resources_to_sell, can_trade = False):
+    def __init__(self, accounts, storage_room, resources_to_buy = None, resources_to_sell = None, is_docked = False):
         '''
         Constructor
         '''
         self.accounts = accounts
-        self.stocks = stocks #should be the stocks of the ship
+        self.storage_room = storage_room
         self.resources_to_buy = resources_to_buy #to be defined by the player or the AI
         self.resources_to_sell = resources_to_sell #to be defined by the player or the AI
-	self.can_trade = can_trade #a trader can only trade if he/she is docked to a station/factory/...
+        self.is_docked = self.storage_room().is_docked() #temporary boolean to test wether the trader is at a station or not / to be removed
         
     def __str__(self):
-        return self.accounts + " " + self.stocks + " " + self.resources_to_buy + " " + self.resources_to_sell
+        return self.accounts + " " + self.storage_room + " " + self.resources_to_buy + " " + self.resources_to_sell + " " + self.is_docked
     
     def get_accounts(self):
         return self.accounts
     
-    def get_stocks(self):
-        return self.stocks
+    def get_storage_room(self):
+        return self.storage_room
     
     def get_resources_to_buy(self):
         return self.resources_to_buy
     
     def get_resources_to_sell(self):
         return self.resources_to_sell
-
-    def get_can_trade(self):
-	return self.can_trade
+    
+    def get_is_docked(self):
+        return self.is_docked
     
     def set_accounts(self, accounts):
         self.accounts = accounts
         
-    def set_stocks(self, stocks):
-        self.stocks = stocks
+    def set_storage_room(self, storage_room):
+        self.storage_room = storage_room
         
     def set_resources_to_buy(self, resources_to_buy):
         self.resources_to_buy = resources_to_buy
         
     def set_resources_to_sell(self, resources_to_sell):
         self.resources_to_sell = resources_to_sell
-
-    def set_can_trade(self, can_trade):
-	self.can_trade = can_trade
+        
+    def set_is_docked(self, is_docked):
+        self.is_docked = is_docked
         
     def buy(self, resource, quantity):
-        if self.get_can_trade() and resource.price()*quantity <= self.accounts.get_credits() and quantity + self.stocks.get_stock_for_resource(resource) <= self.stocks.get_max_stock_for_resource(resource):
+        if self.is_docked and resource.price()*quantity <= self.accounts.get_credits() and quantity + self.stocks.get_stock_for_resource(resource) <= self.stocks.get_max_stock_for_resource(resource):
             self.accounts.decrease(resource.price()*quantity)
-            self.stocks.increase_resource(resource, quantity)
+            self.storage_room.get_stocks().increase_resource(resource, quantity)
         else:
             raise Exception("Can not buy this resource.")
         
     def sell(self, resource, quantity):
-        if self.get_can_trade() and quantity < self.stocks.get_stock_for_resource(resource):
+        if self.is_docked and quantity < self.stocks.get_stock_for_resource(resource):
             self.accounts.increase(resource.price()*quantity)
-            self.stocks.decrease_resource(resource, quantity)
+            self.storage_room.get_stocks().decrease_resource(resource, quantity)
         else:
             raise Exception("Can not sell this resource.")
         

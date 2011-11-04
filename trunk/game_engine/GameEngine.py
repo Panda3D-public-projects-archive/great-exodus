22d63c3da4a71d4d971dc87c88be5742b4da07e8
jdubois69@gmail.com <jdubois69@gmail.com@c5b24106-d323-f95c-f5fa-74f5891a77ab>
2011-11-04 13:01:33 +0000

    Restarted project with a new (sane) basis : MVC model. The main loop is running, waiting for gui and game engine. It seems sleep is not accurate enough for a small sleep. Need to investigate/improve this. Next step is the integration of Ben's developments and plugging them into the main loop.

---
+++ trunk/game_engine/GameEngine.py
@@ -0,0 +1,19 @@
+'''
+Created on 4 nov. 2011
+
+@author: JD219546
+'''
+
+class GameEngine(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    def update(self, iteration_number):
+        pass
---
+++ trunk/main.py
@@ -0,0 +1,12 @@
+'''
+Created on 4 nov. 2011
+
+@author: JD219546
+'''
+from controller.Controller import Controller
+
+if __name__ == '__main__':
+    contr = Controller()
+    contr.start_main_loop()
+    
+    pass
